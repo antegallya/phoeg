3340406f5b8b3ac049c0a58f73b796b885f4ee1e
Gauvain Devillez <gauvain.devillez@umons.ac.be>
2016-03-23 14:01:39 +0100

    Added nauty.a and started nautyInterface.hpp

---
+++ src/nautyInterface.hpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include "nauty.h"
+#include <string>
+
+std::string cannonForm(std::string graph, int n, int m)
+{
+    graph array[n*m], canon[n*m];
+    //EMPTYGRAPH(array, m, n);
+    //ADDONEEDGE(array, 0, 1, m);
+    //for (int i = 0; i < n*m; i++)
+    //{
+        //printf("%d ",array[i]);
+    //}
+    //return "";
+}
