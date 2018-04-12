ce4f7cc140b83e3cc4bb9ad7d0639e57d207224e
Pierre Hauweele <1010576+antegallya@users.noreply.github.com>
2018-04-12 15:01:32 +0200

    Multithread output_inv.

--- src/output_inv.cpp
+++ src/output_inv.cpp
@@ -5,13 +5,34 @@
 #include "nauty_interface.hpp"
 
 int main(int argc, const char* argv[]) {
+    const int bufcap = 1000000;
+    std::vector<std::string> sigs;
+    std::vector<long> values;
+    sigs.reserve(bufcap);
+    values.reserve(bufcap);
     std::string sig;
-    long v;
-    for (sig; std::getline(std::cin, sig);)
-    {
-        phoeg::Graph g = phoeg::convertFromGraph6(sig);
-        v = phoeg::semiTotalDominationNumber(g);
-        if (v != INF)
-            std::cout << sig << "\t" << v << "\n";
-  }
+    bool cont = true;
+    while (cont) {
+        /* Fill the buffer */
+        int i = 0;
+        sigs.clear();
+        values.clear();
+        for (; i < bufcap && cont; ++i) {
+            cont = std::getline(std::cin, sig).good();
+            sigs.push_back(sig);
+            values.push_back(INF);
+        }
+        /* Buffer is full. Process it */
+        #pragma omp parallel for shared(sigs, values)
+        for (int j = 0; j < sigs.size(); ++j) {
+            phoeg::Graph g = phoeg::convertFromGraph6(sigs[j]);
+            values[j] = phoeg::semiTotalDominationNumber(g);
+        }
+        std::cerr << "Output " << sigs.size() << " values" << std::endl;
+        for (i = 0; i < sigs.size(); ++i) {
+            if (values[i] != INF)
+                std::cout << sigs[i] << "\t" << values[i] << "\n";
+        }
+        std::cerr << "Done" << std::endl;
+    }
 }
